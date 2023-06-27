8ab2e4e47aa5b3f2beef6399998cb7d190c39287
Tuan Vo <64002460+tuanvo2908t@users.noreply.github.com>
2023-06-27 13:29:36 +0700

    update

---
+++ resources/views/ckeditor.blade.php
@@ -0,0 +1,10 @@
+
+<textarea id="editor"></textarea>
+<br>
+<script src="./ckeditor5/build/ckeditor.js"></script>
+<script>
+ClassicEditor
+    .create( document.querySelector( '#editor' ))
+    .then()
+    .catch();
+</script>
