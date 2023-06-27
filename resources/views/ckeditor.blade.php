9cc47d01d2c3fc51f89ee88426f5ca464b34e7b7
Tuan Vo <64002460+tuanvo2908t@users.noreply.github.com>
2023-06-27 11:09:36 +0700

    update

--- resources/views/ckeditor.blade.php
+++ resources/views/ckeditor.blade.php
@@ -3,18 +3,17 @@
 <head>
   <meta charset="utf-8">
   <title>CKEditor</title>
-  <script src="https://cdn.ckeditor.com/ckeditor5/38.0.1/classic/ckeditor.js"></script>
-  
+  <script src="https://cdn.ckeditor.com/ckeditor5/38.0.1/classic/ckeditor.js"></script>  
 </head>
 <body>
     <div id="editor">This is some sample content.</div>
 
-<script src="{{ asset('js/ckeditor.js') }}"></script>
+<!-- <script src="{{ asset('js/ckeditor.js') }}"></script> -->
 <script>
   ClassicEditor
     .create( document.querySelector( '#editor' ),{
-        plugins: [ SourceEditing, /* ... */ ],
-        toolbar: [ 'sourceEditing', /* ... */ ]
+        // plugins: [ SourceEditing, /* ... */ ],
+        // toolbar: [ 'sourceEditing', /* ... */ ]
     } )
     .then( editor => {
         console.log( editor );
