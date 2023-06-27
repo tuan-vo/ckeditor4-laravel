cb89edac24f87fd597ec2e2c3acdc96d1da4eafa
Tuan Vo <64002460+tuanvo2908t@users.noreply.github.com>
2023-06-27 15:28:16 +0700

    update

--- routes/web.php
+++ routes/web.php
@@ -16,6 +16,6 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
-Route::get('/ckeditor-page', function () {
+Route::get('/ckeditor', function () {
     return view('ckeditor');
 });
