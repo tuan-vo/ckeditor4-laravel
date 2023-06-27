336cbcdf267eda58a1e06706a833ed1603f81b80
Tuan Vo <64002460+tuanvo2908t@users.noreply.github.com>
2023-06-27 15:26:40 +0700

    update

--- resources/js/ckeditor.js
+++ resources/js/ckeditor.js
@@ -1,3 +1,33 @@
-console.log("alo");
-window.CKEDITOR = require('ckeditor4');
-// const CKEDITOR = require('ckeditor4');
+import ClassicEditorBase from '@ckeditor/ckeditor5-editor-classic/src/classiceditor';
+import { UploadAdapter } from '@ckeditor/ckeditor5-adapter-ckfinder/src/uploadadapter';
+import { Autoformat } from '@ckeditor/ckeditor5-autoformat/src/autoformat';
+import { Bold, Italic } from '@ckeditor/ckeditor5-basic-styles/src/index'; // Updated import path
+import { BlockQuote } from '@ckeditor/ckeditor5-block-quote/src/blockquote';
+import { Essentials } from '@ckeditor/ckeditor5-essentials/src/index'; // Updated import path
+
+
+export default class ClassicEditor extends ClassicEditorBase {}
+
+ClassicEditor.builtinPlugins = [
+  Essentials,
+  UploadAdapter,
+  Autoformat,
+  Bold,
+  Italic,
+  BlockQuote,
+  // Add other plugins you want to include in the build
+  // ...
+];
+
+ClassicEditor.defaultConfig = {
+  toolbar: {
+    items: [
+      'heading',
+      'bold',
+      // Add or remove toolbar items as needed
+      // ...
+    ],
+  },
+  // Rest of classic editor configuration
+  // ...
+};
--- webpack.mix.js
+++ webpack.mix.js
@@ -1,6 +1,5 @@
 const mix = require('laravel-mix');
 
 mix.js('resources/js/app.js', 'public/js')
-    .js('resources/js/ckeditor.js', 'public/js')
-    .disableSuccessNotifications();
+    .js('resources/js/ckeditor.js', 'public/js');
 
